39bad22fa9bf4fb1ef4785d6354721594a56c7b5
DavideHe <627413116@qq.com>
2025-12-01 20:31:12 +0800

    update config and prompt

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ crawl4ai只支持llmlite支持的api，这会给国内用户带来比较大的�
 仓库工具支持了glm的api（因为它提供了免费的flash版本，目前使用起来指令上基本没问题）
 
 ## 步骤
-1. 安装crawl4ai以及依赖,建议py3.12->[crawl4ai-github](https://github.com/unclecode/crawl4ai)
+1. 安装crawl4ai以及依赖(Crawl4AI 0.6.2),建议py3.12->[crawl4ai-github](https://github.com/unclecode/crawl4ai)
 2. 去glm注册账号，获取api_key -> [glm-4 文档](https://bigmodel.cn/dev/activities/free/glm-4-flash)
 3. 运行`main.py`,里面有两个demo，一个是通过网页获取信息，json格式返回，另一个是爬取网页的text内容。都试了下，flash的准确度还可以。
 
--- main.py
+++ main.py
@@ -51,15 +51,24 @@ async def main1():
             llm_config = LLMConfig(provider=provider, api_token=os.getenv('GLM_API_KEY'),base_url=base_url), 
             # schema=OpenAIModelFee.model_json_schema(),
             extraction_type="text",   ## extraction_type: "block" or "schema" or text.
-            instruction="""请帮我提取出网页中间的正文的全部信息，不包含广告、评论和网页两侧的其他信息。
+            instruction="""上面是一篇网页爬取的html内容整理成markdown的形式，其中包含广告和其他无用的信息，请帮我去除掉。
+
 要求：
-其中的图片链接和非广告链接不要省略
-如果涉及到链接不要插入空格、换行等会使链接失效的操作
-信息以markdown文本的格式展示
+- 提取的信息以markdown文本的格式展示正文的层次结构
+- 图片需要严格按照![xxx](xxxxxx)格式整理，图片信息单独成行显示 
+- 先识别出哪些内容是渲染在网页两边的，哪些是渲染在中间的；正文一般是中间部分的内容，请帮我提取这部分正文内容，正文部分可能包含广告请帮我过滤掉这些内容
+- 底部要是有评论，请保留，用户头像可以用 ![用户名](图像链接) 的markdown形式生成
+- 正文中间和正文相关的引用的图片链接和非广告链接不能省略,不要遗漏，以 ![图片title](图片链接) 的markdown形式生成
+- 代码示例如果为了网页渲染需求有不规范的语法内容请帮我整理成合规的语法(一般是去除掉一些无用的渲染字符)
+- 表格采用markdown的表格展示，表格信息不能省略
 
 示例:
 ```markdown
-正文信息
+提取的正文信息
+![图片title](图片链接)
+```python
+python code
+```
 ```
 """
         ),            
@@ -68,13 +77,13 @@ async def main1():
     
     async with AsyncWebCrawler(config=browser_config) as crawler:
         result = await crawler.arun(
-            url='https://blog.csdn.net/qq_36387683/article/details/80578480',
+            # url='https://blog.csdn.net/qq_36387683/article/details/80578480',
+            url='https://data.eastmoney.com/bkzj/BK0729.html',
             config=run_config
         )
         # print(result.extracted_content)
         print(type(result.extracted_content))
         print(json.loads(result.extracted_content)[0]["content"])
-
 
 if __name__ == "__main__":
     asyncio.run(main1())
